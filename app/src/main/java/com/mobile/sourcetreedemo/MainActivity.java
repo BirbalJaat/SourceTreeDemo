9492804a20976730cf7bcc2d9680f0dc8414ea9f
Birbal <birbal.ram@mobileprogramming.com>
2021-12-14 13:43:09 +0530

    Mainactivity log added

--- app/src/main/java/com/mobile/sourcetreedemo/MainActivity.java
+++ app/src/main/java/com/mobile/sourcetreedemo/MainActivity.java
@@ -6,6 +6,7 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -43,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Log.d("MainActivity", "testing for github");
     }
 
     @Override
